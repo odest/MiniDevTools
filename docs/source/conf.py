98d76493218c2241e2c196b4f59fb3652ba2f227
dest <destrochloridium@gmail.com>
2024-03-27 22:11:10 +0300

    Add document files
    Added files generated with Sphinx for uploading to ReadTheDocs in the docs directory.

---
+++ docs/source/conf.py
@@ -0,0 +1,79 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+import os
+import sys
+
+current_dir = os.path.dirname(__file__)
+target_dir = os.path.abspath(os.path.join(current_dir, "../.."))
+sys.path.insert(0, target_dir)
+
+project = "MiniDevTools"
+copyright = "2024, odest"
+author = "odest"
+release = "0.0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
+html_logo = "_static/logo.png"
+
+html_theme_options = {
+    "sidebar_hide_name": False,
+    "light_css_variables": {
+        "body_background_color": "#FFFDE4",
+        "color-brand-primary": "#0000FF",
+        "color-brand-content": "#0000FF",
+    },
+    "dark_css_variables": {
+        "body_background_color": "#00021b",
+        "color-brand-primary": "#00FFFF",
+        "color-brand-content": "#00FFFF",
+    },
+    "footer_icons": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/odest/MiniDevTools",
+            "html": """
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
+                </svg>
+            """,
+            "class": "",
+        },
+    ],
+    "source_repository": "https://github.com/odest/MiniDevTools",
+    "source_branch": "master",
+    "source_directory": "docs/",
+}
+
+html_show_sourcelink = True
+html_title = "MiniDevTools"
+html_favicon = "_static/logo.png"
+html_css_files = [
+    "css/fancybox.css",
+    "css/custom.css",
+]
+html_js_files = [
+    "js/fancybox.umd.js",
+    "js/fancybox.js",
+]
+
+copybutton_prompt_text = (
+    r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
+)
+copybutton_prompt_is_regexp = True
---
+++ docs/source/index.rst
@@ -0,0 +1,68 @@
+.. MiniDevTools documentation master file, created by
+   sphinx-quickstart on Wed Mar 27 15:33:41 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+.. raw:: html
+
+   <p align="center">
+      <img width="10%" align="center" src="./_static/logo.png" alt="logo">
+   </p>
+
+   <h1 align="center">
+      MiniDevTools
+   </h1>
+
+   <hr/>
+
+   <p align="center">
+      A Swiss Army knife for developers
+   </p>
+
+   <p align="center">
+      <a style="text-decoration:none">
+         <img src="https://img.shields.io/badge/python-3.12.0-green" alt="python"/>
+      </a>
+
+      <a style="text-decoration:none">
+         <img src="https://img.shields.io/badge/version-0.0.1-orange" alt="version"/>
+      </a>
+
+      <a style="text-decoration:none">
+         <img src="https://img.shields.io/badge/License-GPLv3-blue?color=#4ec820" alt="GPLv3"/>
+      </a>
+   </p>
+
+   <p align="center">
+      <img src="./_static/banner.png" alt="Interface">
+   </p>
+
+
+MiniDevTools Documentation
+========================================
+This document will show you all the features of MiniDevTools and the best practice of it.
+
+About
+~~~~~
+**MiniDevTools** is a developer tools project designed to empower users to manage their data directly on their own computers without the need to share it with others. It offers a seamless experience for users to handle tasks easily, quickly, and offline.
+
+Features
+~~~~~~~~
+- **Privacy-focused**: With **MiniDevTools**, users can work with their data locally, ensuring their privacy and security are maintained at all times.
+
+- **Offline functionality**: Whether you're in a remote location or experiencing internet connectivity issues, **MiniDevTools** ensures that you can continue your work without interruption.
+
+- **Open Source**: **MiniDevTools** is open source, allowing you to inspect and modify the code according to your requirements, and contribute to its development.
+
+- **Cross-Platform Compatibility**: Whether you're using Windows, macOS, or Linux, **MiniDevTools** works seamlessly across different operating systems.
+
+- **Efficiency**: Perform tasks efficiently and effectively with **MiniDevTools**' intuitive interface and streamlined processes.
+
+- **Customizable**: Tailor **MiniDevTools** to suit your specific needs with customizable options and settings.
+
+- **Developer-friendly**: Built with developers in mind, **MiniDevTools** offers a range of features and functionalities to support your workflow.
+
+- **Flexible Usage**: **MiniDevTools** provides versatile usage options, including a Command Line Interface (CLI), Graphical User Interface (GUI), and seamless Package Integration into Python projects.
+
+.. toctree::
+   :maxdepth: 2
